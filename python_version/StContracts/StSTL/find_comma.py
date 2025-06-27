3c772872dcd6c04c22edb3b1e7b0b04e14bc3587
brensinc <143752685+brensinc@users.noreply.github.com>
2025-06-27 15:21:18 +0200

    Add automatic MATLAB to Python translation

---
+++ python_version/StContracts/StSTL/find_comma.py
@@ -0,0 +1,24 @@
+# Auto-translated from ./StContracts/StSTL/find_comma.m
+
+def find_comma(input_str):
+    # TODO: define outputs: comma_index,comma_num
+#FIND_COMMA Find locations of commas that separates the parameters in the 
+# input string.
+#
+#   Written by Jiwei Li
+
+n = length(input_str)
+comma_index = zeros(1,n)
+comma_num = 0
+left_par = strfind(input_str,'(')
+right_par = strfind(input_str,')')
+for i = 1:n
+    if sum(left_par <= i) == sum(right_par <= i)  and  strcmp(input_str(i),',')
+        comma_num = comma_num + 1
+        comma_index(comma_num) = i
+# end
+# end
+comma_index = comma_index(1:comma_num)
+
+# end
+
